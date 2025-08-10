c8a49317a64bf48025fb6ed03aa47f0218445cec
ronlemire2 <ronlemire2@gmail.com>
2025-08-10 09:33:25 -0700

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Ron Lemire on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
