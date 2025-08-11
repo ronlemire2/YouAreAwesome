03f00564f57cec6fb05b9288ceb02e687ad41141
ronlemire2 <ronlemire2@gmail.com>
2025-08-11 13:35:56 -0700

    Add Awesome button, Great button, spacers

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,25 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I Am A Programmer!"
+    @State private var message = "Awesome!"
     
     var body: some View {
-        
         VStack {
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click Me!") {
-                message = "Awesome!"
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                Button("Great") {
+                    message = "Great!"
+                }
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
-        
     }
 }
 
