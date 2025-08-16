999b6f557a628d8281f6367fa87ef5dcb27d7216
ronlemire2 <ronlemire2@gmail.com>
2025-08-15 17:20:36 -0700

    Ch. 1.11 Randomization in Swift(SwiftUI Course 2025)

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -36,23 +36,13 @@ struct ContentView: View {
             Button("Show Message") {
                 let messages = ["You are Awesome!",
                                 "When the Genius Bar Needs Help, They Call You!",
-                                //"Gadzooks my friend! I am astonished at how utterly magnificient you are!",
                                 "You are Great!",
                                 "You are Fantastic!",
                                 "Fabulous? That's You!",
                                 "You Make Me Smile!"
                                ]
-                message = messages[messageNumber]
-                messageNumber += 1
-                if messageNumber > messages.count-1 {
-                    messageNumber = 0
-                }
-                
-                imageName = "image\(imageNumber)"
-                imageNumber += 1
-                if imageNumber > 9 {
-                    imageNumber = 0
-                }
+                message = messages[Int.random(in: 0...messages.count - 1)]
+                imageName = "image\(Int.random(in: 0...9))"
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
